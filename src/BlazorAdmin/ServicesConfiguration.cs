ba76a739addb4110f81318f54cc4bdcfc8fcd3ba
kalleman1 <kaper1@live.dk>
2024-11-11 19:21:42 +0100

    Removed use of cached items

--- src/BlazorAdmin/ServicesConfiguration.cs
+++ src/BlazorAdmin/ServicesConfiguration.cs
@@ -9,11 +9,14 @@ public static class ServicesConfiguration
 {
     public static IServiceCollection AddBlazorServices(this IServiceCollection services)
     {
-        services.AddScoped<ICatalogLookupDataService<CatalogBrand>, CachedCatalogLookupDataServiceDecorator<CatalogBrand, CatalogBrandResponse>>();
+        //Vi bruger CatalogLookupDataService her i stedet for cached, da der ikke er behov for at benytte localstorage (for lidt tid)
+        services.AddScoped<ICatalogLookupDataService<CatalogBrand>, CatalogLookupDataService<CatalogBrand, CatalogBrandResponse>>();
         services.AddScoped<CatalogLookupDataService<CatalogBrand, CatalogBrandResponse>>();
-        services.AddScoped<ICatalogLookupDataService<CatalogType>, CachedCatalogLookupDataServiceDecorator<CatalogType, CatalogTypeResponse>>();
+        // Vi gør det samme her, da der ikke er behov for at benytte localstorage
+        services.AddScoped<ICatalogLookupDataService<CatalogType>, CatalogLookupDataService<CatalogType, CatalogTypeResponse>>();
         services.AddScoped<CatalogLookupDataService<CatalogType, CatalogTypeResponse>>();
-        services.AddScoped<ICatalogItemService, CachedCatalogItemServiceDecorator>();
+        // Vi gør det samme her, da der ikke er behov for at benytte localstorage
+        services.AddScoped<ICatalogItemService, CatalogItemService>();
         services.AddScoped<CatalogItemService>();
 
         return services;
