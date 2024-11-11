69dc4b1cc60356a982dfbd3bd28fd865fe5b7ae7
kalleman1 <kaper1@live.dk>
2024-11-11 19:24:09 +0100

    Added jsonproperties to map correctly to response

--- src/BlazorShared/Models/CatalogBrand.cs
+++ src/BlazorShared/Models/CatalogBrand.cs
@@ -6,7 +6,11 @@ namespace BlazorShared.Models;
 [Endpoint(Name = "catalog-brands")]
 public class CatalogBrand : LookupData
 {
-    //Test
-    [JsonPropertyName("brand")] // Map "brand" from JSON to "Name"
-    public new string Name { get; set; }
+    // Vi angiver navnet på json properties, så vi kan lave dem om til C# objekter
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("brand")]
+    public string Name { get; set; }
+    
 }
